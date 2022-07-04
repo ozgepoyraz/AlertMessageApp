5d29a35e12a5ee1bdb630ab1b965b9d564499d87
Özge Poyraz <71945096+ozgepoyraz@users.noreply.github.com>
2022-07-04 18:19:44 +0300

    commit

--- AlertMessageApp/ViewController.swift
+++ AlertMessageApp/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userID: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    @IBOutlet weak var userPasswordAgain: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonClicked(_ sender: Any) {
+        
+      
+        
+        if userPassword.text == "" || userPasswordAgain.text == "" || userID.text == ""{
+            makeAlert(title: "Error", message: "Fields can not be left blank!")
+        }
+        
+        if userPassword.text != userPasswordAgain.text{
+            makeAlert(title: "Error", message: "Password do not match!")
+        }
+        
+    }
+    
+    func makeAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: title, style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+        
+    }
+    
 }
 
